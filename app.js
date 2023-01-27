30634fa8a0b9b972bf6dc0afbdaaaeb0c7675aa3
Kartik Gupta <kartikgupta7267@gmail.com>
2023-01-27 23:06:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,99 +1,92 @@
-const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
-const dotenv = require('dotenv');
-const mongodb = require('mongodb');
-const express = require('express');
-dotenv.config();
-
-const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${process.env.WEATHER_API_KEY}&units=metric`
-const mongodbUrl = process.env.MONGO_URL;
-const telegramBotToken = process.env.BOT_TOKEN
-const URI = `/webhook/${telegramBotToken}`
-
-mongodb.MongoClient.connect(mongodbUrl, { useUnifiedTopology: true }, (err, client) => {
-  if (err) throw err;
-  const db = client.db('telegram-bot');
-  const subscribers = db.collection('subscribers');
+/**
+ * This example demonstrates using polling.
+ * It also demonstrates how you would process and send messages.
+ */
 
 
-  const bot = new TelegramBot(telegramBotToken, { polling: true });
-
-
-  bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, `Welcome to my Telegram bot!
-Use the /subscribe command to subscribe to temperature updates.
-Use the /unsubscribe command to unsubscribe.
-Use the /currenttemp command to check the current temperature in Delhi.
-Use the /subscribers command to see the list of subscribed users.
-    `);
-  });
-
-
-  bot.onText(/\/subscribe/, (msg) => {
-    subscribers.findOne({ chatId: msg.chat.id }, (err, subscriber) => {
-      if (err) throw err;
-      if (subscriber === null) {
-        subscribers.insertOne({
-          chatId: msg.chat.id,
-          firstName: msg.chat.first_name,
-          lastName: msg.chat.last_name,
-          username: msg.chat.username
-        }, (err) => {
-          if (err) throw err;
-          bot.sendMessage(msg.chat.id, 'You have been subscribed to temperature updates.');
-        });
-      } else {
-        bot.sendMessage(msg.chat.id, 'You are already subscribed to temperature updates.');
-      }
-    });
-  });
-
-
-  bot.onText(/\/unsubscribe/, (msg) => {
-    subscribers.deleteOne({ chatId: msg.chat.id }, (err) => {
-      if (err) throw err;
-      bot.sendMessage(msg.chat.id, 'You have been unsubscribed from temperature updates.');
-    });
-  });
-
-  bot.onText(/\/currenttemp/, (msg) => {
-    request(weatherUrl, (err, res, body) => {
-      if (err) throw err;
-      const weather = JSON.parse(body);
-      const temperature = weather.main.temp;
-      bot.sendMessage(msg.chat.id, `The current temperature in Delhi is ${temperature}°C.`);
-    });
-  });
-
-
-  bot.onText(/\/subscribers/, (msg) => {
-    subscribers.find({}).toArray((err, subscribers) => {
-      if (err) throw err;
-      if (subscribers.length === 0) {
-        bot.sendMessage(msg.chat.id, 'There are no subscribers.');
-      } else {
-        let subscribersList = 'List of subscribers:\n';
-        subscribers.forEach((subscriber) => {
-          subscribersList += `- ${subscriber.firstName} ${subscriber.lastName} (@${subscriber.username}) \n`;
-        });
-        bot.sendMessage(msg.chat.id, subscribersList);
-      }
-    });
-  });
-  function getTemperature() {
-    request(weatherUrl, function (err, res, body) {
-      if (!err && res.statusCode === 200) {
-        const weather = JSON.parse(body);
-        const temperature = weather.main.temp;
-        const message = `The current temperature in Delhi is${temperature}°C.`;
-        subscribers.find({}).toArray((err, docs) => {
-          if (err) throw err;
-          docs.forEach((subscriber) => {
-            bot.sendMessage(subscriber.chatId, message);
-          });
-        });
-      }
-    });
-  }
-  setInterval(getTemperature, 3600000);
-});
+ const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
+ const TelegramBot = require('..');
+ const request = require('request');
+ const options = {
+   polling: true
+ };
+ const bot = new TelegramBot(TOKEN, options);
+ 
+ 
+ // Matches /photo
+ bot.onText(/\/photo/, function onPhotoText(msg) {
+   // From file path
+   const photo = `${__dirname}/../test/data/photo.gif`;
+   bot.sendPhoto(msg.chat.id, photo, {
+     caption: "I'm a bot!"
+   });
+ });
+ 
+ 
+ // Matches /audio
+ bot.onText(/\/audio/, function onAudioText(msg) {
+   // From HTTP request
+   const url = 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg';
+   const audio = request(url);
+   bot.sendAudio(msg.chat.id, audio);
+ });
+ 
+ 
+ // Matches /love
+ bot.onText(/\/love/, function onLoveText(msg) {
+   const opts = {
+     reply_to_message_id: msg.message_id,
+     reply_markup: JSON.stringify({
+       keyboard: [
+         ['Yes, you are the bot of my life ❤'],
+         ['No, sorry there is another one...']
+       ]
+     })
+   };
+   bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
+ });
+ 
+ 
+ // Matches /echo [whatever]
+ bot.onText(/\/echo (.+)/, function onEchoText(msg, match) {
+   const resp = match[1];
+   bot.sendMessage(msg.chat.id, resp);
+ });
+ 
+ 
+ // Matches /editable
+ bot.onText(/\/editable/, function onEditableText(msg) {
+   const opts = {
+     reply_markup: {
+       inline_keyboard: [
+         [
+           {
+             text: 'Edit Text',
+             // we shall check for this value when we listen
+             // for "callback_query"
+             callback_data: 'edit'
+           }
+         ]
+       ]
+     }
+   };
+   bot.sendMessage(msg.from.id, 'Original Text', opts);
+ });
+ 
+ 
+ // Handle callback queries
+ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+   const action = callbackQuery.data;
+   const msg = callbackQuery.message;
+   const opts = {
+     chat_id: msg.chat.id,
+     message_id: msg.message_id,
+   };
+   let text;
+ 
+   if (action === 'edit') {
+     text = 'Edited Text';
+   }
+ 
+   bot.editMessageText(text, opts);
+ });
