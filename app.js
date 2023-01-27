5cf500fce5683c310d77cc4886d6f6a523e593e4
Kartik Gupta <kartikgupta7267@gmail.com>
2023-01-27 22:37:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,21 @@ const WEBHOOK_URL = SERVER_URL + URI
 const app = express()
 app.use(bodyParser.json())
 
-const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
-    console.log(res.data)
-}
 
+app.post(URI, async (req, res) => {
+    console.log(req.body)
 
+    const chatId = req.body.message.chat.id
+    const text = req.body.message.text
+
+    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+        chat_id: chatId,
+        text: text
+    })
+    return res.send()
+})
 
 app.listen(process.env.PORT || 5000, async () => {
     console.log('🚀 app running on port', process.env.PORT || 5000)
-    await init()
+
 })
