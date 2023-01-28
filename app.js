12d4dfa49680128ed386d5aa131dae80bbe73a98
Kartik Gupta <kartikgupta7267@gmail.com>
2023-01-28 09:20:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,111 @@
-const { Bot } = require("grammy");
-
+const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
 const dotenv = require('dotenv');
-
+const mongodb = require('mongodb');
+const express = require('express');
 dotenv.config();
-const TOKEN = process.env.TELEGRAM_TOKEN 
-// Create an instance of the `Bot` class and pass your authentication token to it.
-const bot = new Bot(TOKEN); // <-- put your authentication token between the ""
 
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
+const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${process.env.WEATHER_API_KEY}&units=metric`
+const mongodbUrl = process.env.MONGO_URL;
+const telegramBotToken = process.env.BOT_TOKEN
 
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+mongodb.MongoClient.connect(mongodbUrl, { useUnifiedTopology: true }, (err, client) => {
+  if (err) throw err;
+  const db = client.db('telegram-bot');
+  const subscribers = db.collection('subscribers');
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
 
-// Start the bot.
-bot.start();
+  const bot = new TelegramBot(telegramBotToken, { polling: true });
+
+
+  bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, `Welcome to my Telegram bot!
+Use the /subscribe command to subscribe to temperature updates.
+Use the /unsubscribe command to unsubscribe.
+Use the /currenttemp command to check the current temperature in Delhi.
+Use the /subscribers command to see the list of subscribed users.
+    `);
+  });
+
+
+  bot.onText(/\/subscribe/, (msg) => {
+    subscribers.findOne({ chatId: msg.chat.id }, (err, subscriber) => {
+      if (err) throw err;
+      if (subscriber === null) {
+        subscribers.insertOne({
+          chatId: msg.chat.id,
+          firstName: msg.chat.first_name,
+          lastName: msg.chat.last_name,
+          username: msg.chat.username
+        }, (err) => {
+          if (err) throw err;
+          bot.sendMessage(msg.chat.id, 'You have been subscribed to temperature updates.');
+        });
+      } else {
+        bot.sendMessage(msg.chat.id, 'You are already subscribed to temperature updates.');
+      }
+    });
+  });
+
+
+  bot.onText(/\/unsubscribe/, (msg) => {
+    subscribers.deleteOne({ chatId: msg.chat.id }, (err) => {
+      if (err) throw err;
+      bot.sendMessage(msg.chat.id, 'You have been unsubscribed from temperature updates.');
+    });
+  });
+
+  bot.onText(/\/currenttemp/, (msg) => {
+    request(weatherUrl, (err, res, body) => {
+      if (err) throw err;
+      const weather = JSON.parse(body);
+      const temperature = weather.main.temp;
+      bot.sendMessage(msg.chat.id, `The current temperature in Delhi is ${temperature}°C.`);
+    });
+  });
+
+
+  bot.onText(/\/subscribers/, (msg) => {
+    subscribers.find({}).toArray((err, subscribers) => {
+      if (err) throw err;
+      if (subscribers.length === 0) {
+        bot.sendMessage(msg.chat.id, 'There are no subscribers.');
+      } else {
+        let subscribersList = 'List of subscribers:\n';
+        subscribers.forEach((subscriber) => {
+          subscribersList += `- ${subscriber.firstName} ${subscriber.lastName} (@${subscriber.username}) \n`;
+        });
+        bot.sendMessage(msg.chat.id, subscribersList);
+      }
+    });
+  });
+  function getTemperature() {
+    request(weatherUrl, function (err, res, body) {
+      if (!err && res.statusCode === 200) {
+        const weather = JSON.parse(body);
+        const temperature = weather.main.temp;
+        const message = `The current temperature in Delhi is${temperature}°C.`;
+        subscribers.find({}).toArray((err, docs) => {
+          if (err) throw err;
+          docs.forEach((subscriber) => {
+            bot.sendMessage(subscriber.chatId, message);
+          });
+        });
+      }
+    });
+  }
+  setInterval(getTemperature, 3600000);
+
+
+  const app = express();
+  app.get('/subscribers', (req, res) => {
+    subscribers.find({}).toArray((err, subscribers) => {
+      if (err) throw err;
+      res.json(subscribers);
+    });
+  });
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
+
+});
